1853aa85bd485ca048903a5d5c0eae1ae65ef2fb
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-12-06 12:24:47 +0400

    Add Application.java

---
+++ java-cache-service/src/main/java/com/example/weather/Application.java
@@ -0,0 +1,8 @@
+package com.example.weather;
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO start HTTP server and register handlers
+        System.out.println("Weather cache service starting...");
+    }
+}
