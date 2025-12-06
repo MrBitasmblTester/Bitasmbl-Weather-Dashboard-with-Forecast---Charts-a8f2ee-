e65612efca84d1a9215664d9307aa84f0f24b4dd
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-12-06 12:24:48 +0400

    Add WeatherController.java

---
+++ java-cache-service/src/main/java/com/example/weather/WeatherController.java
@@ -0,0 +1,9 @@
+package com.example.weather;
+
+public class WeatherController {
+    // TODO: map GET /weather?city=
+    public String getWeather(String city) {
+        // lookup cache, or call WeatherService
+        return "{}";
+    }
+}
